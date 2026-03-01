f82558f15bd3ab165ab60b59177ad12a602469ec
Pawan <imrealpawan@gmail.com>
2026-03-01 18:40:39 +0530

    fix: minor fixes

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -18,7 +18,8 @@ export const env = createEnv({
     AWS_ACCESS_KEY_ID: z.string(),
     AWS_SECRET_ACCESS_KEY: z.string(),
     AWS_REGION: z.string(),
-    AWS_S3_BUCKET_NAME: z.string(),s
+    AWS_S3_BUCKET_NAME: z.string(),
+    MODAL_ZIMAGE_URL: z.string().url(),
   },
 
   /**
@@ -48,6 +49,7 @@ export const env = createEnv({
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_REGION: process.env.AWS_REGION,
     AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+    MODAL_ZIMAGE_URL: process.env.MODAL_ZIMAGE_URL,
 
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 
